4b1b6ed41cbd0b10b8c63d16d22b5e6b2f18b25b
sony-vedom <minerva.lankmiller@gmail.com>
2024-07-13 02:22:15 +0500

    Update token

--- src/shared/config/api/const.ts
+++ src/shared/config/api/const.ts
@@ -1,4 +1,4 @@
 export const API_URL = "https://api.github.com/graphql"
 
-// Public Token на 30 дней действует, только для публичных repo
-export const TOKEN = "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"
+// Public Token для тестирования
+export const TOKEN = "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"
