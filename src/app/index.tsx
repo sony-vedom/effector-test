ab0cb1cc2ca33042bb86edda62ab8e1e16d70e27
sony-vedom <minerva.lankmiller@gmail.com>
2024-07-13 01:58:19 +0500

    app started from root

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,4 @@
-import {appStarted} from "@/shared/config/initialization";
 import {Providers} from "./providers";
-
-appStarted()
 
 export function App() {
     return (
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './app'
 import './index.css'
+import {appStarted} from "@/shared/config/initialization";
+
+appStarted()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
